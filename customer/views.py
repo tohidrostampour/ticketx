a8af1d27de7912b3218408ff02ad45ccf05b22fc
tohidrostampour <rostampourtwhid@gmail.com>
2022-05-26 14:27:44 +0430

    feat: added permissions for customer, ticket and order

--- customer/views.py
+++ customer/views.py
@@ -1,12 +1,15 @@
-from rest_framework.exceptions import MethodNotAllowed
 from rest_framework.viewsets import ModelViewSet
 
 from customer.models import Customer
 from customer.serializers import CustomerSerializer
+from customer.permissions import IsCustomerOrAdmin
 
 
 class CustomerViewSet(ModelViewSet):
     queryset = Customer.objects.all()
     serializer_class = CustomerSerializer
     http_method_names = ['get', 'put', 'patch', 'delete']
+    permission_classes = [IsCustomerOrAdmin]
 
+    def get_queryset(self):
+        return self.queryset.filter(user=self.request.user)
