67428c700a3bd9267404b6f8685d1ebf36033818
nimilioum <nimilioum@gmail.com>
2022-05-31 00:40:33 +0430

    fix: fixed response issue

--- ticket/views.py
+++ ticket/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import get_object_or_404
-from requests import Response
+from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from rest_framework.generics import GenericAPIView
